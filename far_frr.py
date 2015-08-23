2bdcac8b6b9227d8ce2d15a1c4f11ced0bd78b6f
Himanshu Singal <singal_himanshu@yahoo.co.in>
2015-08-23 18:33:28 +0530

    added x

--- far_frr.py
+++ far_frr.py
@@ -1,8 +1,9 @@
-original=[0]*1000001
-imposter=[0]*1000001
+x=1000000
+original=[0]*x
+imposter=[0]*x
 
 with open("L4_1000.txt",'r') as stream:
-    chk=1
+    
     for row in stream:
         data=row.split("\t")
         score = float(data[5].split("\n")[0])*1000000
@@ -17,17 +18,20 @@ with open("L4_1000.txt",'r') as stream:
                     original[intscore]+=1
             else:
                 original[intscore]+=1
+                 
         else:
-            imposter[intscore]+=1
-        if(chk==10):
-            break
-        chk=chk+1   
-for i in range(1,1000001):
+            if intscore==x:
+                imposter[intscore-1]+=1
+            else:
+                imposter[intscore]+=1
+          
+           
+for i in range(1,x):
     imposter[i]=imposter[i-1]+imposter[i]
     
 
     
-for i in range(1000001-2,-1,-1):
+for i in range(x-2,-1,-1):
     original[i]=original[i+1]+original[i]
 
 print imposter
